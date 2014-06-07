3274234f4c4dc0d22094aed58e69cdcc0a9ee438
Andrew Hao <ahao@blurb.com>
2014-06-06 22:44:46 -0700

    Tweak test logging and speed.

--- test/test_helper.js
+++ test/test_helper.js
@@ -10,15 +10,11 @@ var specs = [
   'chordmeister/classified_line_spec.js',
 ];
 
-console.log("here");
-
 require(specs, function(require) {
-  console.log("in the spec require block");
   mocha.setup('bdd');
   expect = chai.expect;
   // Why? Some async loading condition? Is there a cb I should be hooking into?
   setTimeout(function() {
-    console.log("almost there")
     mocha.run();
-  }, 1000);
+  }, 100);
 })
